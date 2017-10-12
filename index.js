b861a283876dc66c34e2f06ba3d416ec6f98e916
takuya0206 <tokiwatakuya@gmail.com>
2017-10-12 01:04:44 +0000

    フォーマットの修正

--- index.js
+++ index.js
@@ -5,35 +5,39 @@ var masterTable = ss.getSheetByName('masterTable');
 var resource = ss.getSheetByName('resource');
 var schedule = ss.getSheetByName('schedule');
 var EVM = ss.getSheetByName('EVM');
-var PND = ss.getSheetByName('PND');
+var PDM = ss.getSheetByName('PDM');
 if(!masterTable){masterTable = ss.insertSheet('masterTable', 0);}
 if(!resource){resource = ss.insertSheet('resource', 1);}
 if(!schedule){schedule = ss.insertSheet('schedule', 2);}
 if(!EVM){EVM = ss.insertSheet('EVM', 3);}
-if(!PND){PND = ss.insertSheet('PND', 4)}
+if(!PDM){PDM = ss.insertSheet('PDM', 4)}
 
-//マスターテーブル情報取得
-var firstRow = masterTable.getRange('1:1');
-var firstRowNum = firstRow.getNumColumns();
+//スケジュールシート情報取得
+var firstRow = schedule.getRange('1:1')
+var RowNum = firstRow.getNumColumns();
 
 
 //フォーマット用アセット
 var items = [
-  ['No.', 'タスク名', '開始日', '終了日', '工数 （人日）', '進捗'],
-  ['wbs', 'tasks', 'start', 'finish', 'workload', 'progress']
+  ['No.', 'タスク名', '予定開始', '予定終了', '予定工数', '実際開始', '実際終了', '実際工数', '担当', '進捗'],
+  ['wbs', 'tasks','planedStart', 'planedFinish', 'planedWorkload', 'actualStart', 'actualFinish', 'actualWorkload', 'responsiblity', 'progress']
 ];
-
-var wbsColumnName = 0;
-var wbsCodeName = 1;
 var wbsColumnNameLength = items[0].length;
 
 
 function onOpen() {
-  init();
   //WBSの情報がなければガントチャート追加
-  if(!masterTable.getRange('B4').getValue()) {
-    var today = Moment.moment().format('YYYY-MM-DD');
-    formatGantchart(7, today);
+  if(!schedule.getRange('B4').getValue()) {
+    //月曜スタートになるよう調整
+    var today = Moment.moment();
+    var tmp = 0;
+    while (Math.abs(today.day()) + tmp <= 7) {
+     tmp++;
+      };
+     var monday = today.add(tmp, 'days');
+
+    init();
+    formatGantchart(7, monday.format('YYYY/MM/DD'));
   };
 
   //カスタムメニューをUIに追加
@@ -56,13 +60,18 @@ function showSidebar() {
 
 //初期フォーマット
 function init(){
-  masterTable.setFrozenRows(2)
-  var wbsColumnRange = masterTable.getRange(2, 1, 2, wbsColumnNameLength);
+  schedule.setFrozenRows(1);
+  schedule.setFrozenColumns(2);
+  schedule.hideRows(2);
+  schedule.setRowHeight(1, 31.5);
+  firstRow.setBackground('#f3f3f3');
+  var wbsColumnRange = schedule.getRange(1, 1, 2, wbsColumnNameLength);
   wbsColumnRange.setValues(items);
-  masterTable.hideRows(3)
 
-  if (firstRowNum < 30) {
-    masterTable.insertColumns(wbsColumnNameLength+1, 200);
+  // 列の追加
+  if (RowNum < 30) {
+    schedule.insertColumns(wbsColumnNameLength+1, 180);
+    RowNum = schedule.getRange('1:1').getNumColumns();
   }
 };
 
@@ -71,14 +80,17 @@ function formatGantchart(span, date) {
   var line_column = wbsColumnNameLength+1;
   var date = Moment.moment(date);
   //列幅
-  for (var i = wbsColumnNameLength+1; firstRowNum >= i; i++) {
-    masterTable.setColumnWidth(i, 25);
+  for (var i = wbsColumnNameLength+1; RowNum >= i; i++) {
+    schedule.setColumnWidth(i, 25);
+    if ((i - wbsColumnNameLength) % 7 === 0) {
+      schedule.getRange(2, i-1, 1000, 2).setBackground('#fcefe3');
+    }
   };
   //枠線と日付
-  while (firstRowNum >= line_column){
-   var range = masterTable.getRange(1, line_column, 1000, line_column+span);
+  while (RowNum >= line_column){
+   var range = schedule.getRange(1, line_column, 1000, line_column+span);
    range.setBorder(null, true, null, true, false, false, "black", SpreadsheetApp.BorderStyle.SOLID);
-   var range = masterTable.getRange(2, line_column, 1, 1);
+   var range = schedule.getRange(1, line_column, 1, 1);
    range.setValue('(' + date.format('YYYY/MM/DD') + ')');
    line_column += span;
    date = date.add(span, 'days');
